28e202bdcc01d15d335e06d6046b7b92581790ad
Carlos Martin <alu0100702293@ull.edu.es>
2013-10-17 17:50:35 +0100

    ejecutar racional.rb para probar,implementado un nuevo metodo gcd

--- racional.rb
+++ racional.rb
@@ -1,14 +1,14 @@
 # Implementar en este fichero la clase para crear objetos racionales
-
-require "./gcd.rb"
+#! /usr/bin/ruby
+require "./gcd1.rb"
 
 class Fraccion
     ####atributos
     attr_reader :numerador, :denominador
 
     ####métodos
-    def initialize (a,b) #Inicializa los atributos(num,denom)
-        mcd = gcd (a,b)
+    def initialize(a,b) #Inicializa los atributos(num,denom)
+        mcd = gcd(a,b)
         @numerador, @denominador = a/mcd, b/mcd #garantizamos que la fraccion este simplificada
     end
 
@@ -44,3 +44,27 @@ class Fraccion
         Fraccion.new(num/mcd,denom/mcd)
     end
 end
+
+###MAINPRUEBA###
+    fa = Fraccion.new(4,3)
+    fb = Fraccion.new(2,5)
+
+#suma de racionales
+puts "#{fa.to_s} + #{fb.to_s} ="
+fc = fa+fb
+puts "#{fc.to_s}"
+
+#resta de racionales
+puts "#{fa.to_s} - #{fb.to_s} ="
+fc = fa-fb
+puts "#{fc.to_s}"
+
+#producto de racionales
+puts "#{fa.to_s} * #{fb.to_s} ="
+fc = fa*fb
+puts "#{fc.to_s}"
+
+#división de racionales
+puts "(#{fa.to_s}) / (#{fb.to_s}) ="
+fc = fa/fb
+puts "#{fc.to_s}"
