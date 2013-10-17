28b5414d29ce541873e9ce21a49012ba2660a048
Carlos Martin <alu0100702293@ull.edu.es>
2013-10-17 17:14:42 +0100

    clase implementada

--- racional.rb
+++ racional.rb
@@ -3,4 +3,44 @@
 require "./gcd.rb"
 
 class Fraccion
+    ####atributos
+    attr_reader :numerador, :denominador
+
+    ####métodos
+    def initialize (a,b) #Inicializa los atributos(num,denom)
+        mcd = gcd (a,b)
+        @numerador, @denominador = a/mcd, b/mcd #garantizamos que la fraccion este simplificada
+    end
+
+    def to_s #Pone el número racional como cadena para mostrarlo por pantalla
+        "#{numerador}/#{denominador}"
+    end
+
+    def +(op) #Operador suma de racionales
+        denom = @denominador*op.denominador #denominador del resultado
+        num = (@numerador*op.denominador + op.numerador*@denominador) #numerador del resultado
+        mcd = gcd(num,denom) #calculamos el max. comun divisor para simplificar la fraccion creada
+        Fraccion.new(num/mcd,denom/mcd)
+    end
+
+    def -(op) #operador resta de racionales
+        denom = @denominador*op.denominador #denominador del resultado
+        num = (@numerador*op.denominador - op.numerador*@denominador) #numerador del resultado
+        mcd = gcd(num,denom) #calculamos el max. comun divisor para simplificar la fraccion creada
+        Fraccion.new(num/mcd,denom/mcd)
+    end
+
+    def *(op) #operador producto de racionales
+        denom = @denominador*op.denominador
+        num = @numerador*op.numerador
+        mcd = gcd(num,denom)
+        Fraccion.new(num/mcd,denom/mcd)
+    end
+
+    def /(op)
+        denom = @denominador*op.numerador
+        num = @numerador*op.denominador
+        mcd = gcd(num,denom)
+        Fraccion.new(num/mcd,denom/mcd)
+    end
 end
