f5d33d73e1099da9269b61f3a6288f70c0c7c952
Carlos Martin <alu0100702293@ull.edu.es>
2013-10-17 18:42:19 +0100

    Pruebas unitarias completado

--- tc_racional.rb
+++ tc_racional.rb
@@ -9,13 +9,34 @@ $fb =Fraccion.new(2,5)
 $fadd = Fraccion.new(26,15)
 $fdiff = Fraccion.new(14,15)
 $fprod = Fraccion.new(8,15)
-$fdiv = Fraccion.new(10,13)
+$fdiv = Fraccion.new(10,3)
 
 class Test_Fraccion < Test::Unit::TestCase
-    def test_add
-        assert_equal($fadd,$fa + $fb )
-    end
     def test_gcd
         assert_equal(3,gcd(6,3))
+    end
+
+    def test_suma
+        assert_equal($fadd.to_s,($fa+$fb).to_s )
+    end
+
+    def test_diferencia
+        assert_equal($fdiff.to_s,($fa - $fb).to_s )
+    end
+
+    def test_producto
+        assert_equal($fprod.to_s,($fa * $fb).to_s )
+    end
+
+    def test_cociente
+        assert_equal($fdiv.to_s,($fa / $fb).to_s )
+    end
+
+    def test_initialize
+        assert_equal($fa.to_s,Fraccion.new(4,3).to_s)
+    end
+
+    def test_denominadorcero
+        assert_equal("/",Fraccion.new(4,0).to_s)
     end
 end
